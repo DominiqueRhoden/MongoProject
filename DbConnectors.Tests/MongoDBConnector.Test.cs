fa762e6254a99721b0c1025a898b43f5c2fa2a40
DominiqueRhoden <drho4706@students.vsu.edu>
2025-12-07 15:42:46 -0500

    Test: Implement Ping failure case

--- DbConnectors.Tests/MongoDBConnector.Test.cs
+++ DbConnectors.Tests/MongoDBConnector.Test.cs
@@ -9,6 +9,13 @@ namespace DbConnectors.Tests
     public class MongoDBConnectorTest
     {
         [Fact]
+        public void Ping_ShouldReturnFalse_WhenConnectionIsInvalid()
+        {
+            var connector = new MongoDBConnector("mongodb://invalid:27017");
+            var result = connector.Ping();
+            Assert.False(result);
+        }
+
         public async Task Ping_ShouldReturnTrue_WhenMongoIsRunning()
         {
             await using var mongoContainer = new MongoDbBuilder()
