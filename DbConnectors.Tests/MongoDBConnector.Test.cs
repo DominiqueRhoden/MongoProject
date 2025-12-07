82d5d5ca804854339c20338b8774334067389b0f
DominiqueRhoden <drho4706@students.vsu.edu>
2025-12-07 15:35:50 -0500

    Test: Implement Ping success case using test containers

--- DbConnectors.Tests/MongoDBConnector.Test.cs
+++ DbConnectors.Tests/MongoDBConnector.Test.cs
@@ -1,11 +1,28 @@
-﻿namespace DbConnectors.Tests
+﻿using Xunit;
+using DbConnectors;
+using Testcontainers.MongoDb;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+
+namespace DbConnectors.Tests
 {
-    public class UnitTest1
+    public class MongoDBConnectorTest
     {
         [Fact]
-        public void Test1()
+        public async Task Ping_ShouldReturnTrue_WhenMongoIsRunning()
         {
+            await using var mongoContainer = new MongoDbBuilder()
+                .WithImage("mongo:7.0")
+                .Build();
 
+            await mongoContainer.StartAsync();
+
+            var connector = new MongoDBConnector(mongoContainer.GetConnectionString());
+            var result = connector.Ping();
+
+            Assert.True(result);
+
+            await mongoContainer.StopAsync();
         }
     }
 }
